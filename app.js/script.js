22e03b3ec0a09bda7a165283098f10628dae1eb7
dee chao <chaoserady@gmail.com>
2022-05-02 22:47:25 -0500

    added desktop nav version

--- app.js/script.js
+++ app.js/script.js
@@ -4,6 +4,7 @@ const burgerOpen = document.querySelector(".burger-open");
 const burgerClose = document.querySelector(".burger-close");
 const nav = document.querySelector(".nav");
 const navLinks = document.querySelectorAll(".nav-links");
+const hoverLines = document.querySelectorAll(".hoverlines");
 
 // Open Nav Menu Function
 function openNavMenu() {
@@ -33,5 +34,47 @@ burgerClose.addEventListener("click", () => {
 navLinks.forEach((nl) => {
   nl.addEventListener("click", () => {
     closeNavMenu();
+  });
+});
+
+// Reset Links that are not clicked
+
+function resetLinks() {
+  navLinks.forEach((nl) => {
+    nl.classList.remove("activeLink");
+  });
+}
+
+function resetHoverLines() {
+  hoverLines.forEach((hl) => {
+    hl.classList.remove("activeLines");
+  });
+}
+
+function addHoverLines() {
+  hoverLines.forEach((hl) => {
+    hl.classList.add("activeLines");
+  });
+}
+
+// Media Query
+
+const mediaQueryDesktop = window.matchMedia("(min-width: 1224px)");
+
+// Nav Link added to Home Link when in landscape desktop mode
+
+if (mediaQueryDesktop.matches === true) {
+  navLinks[0].classList.add("activeLink");
+  hoverLines[0].classList.add("activeLines");
+}
+
+// Click Event for Links
+
+navLinks.forEach((nl) => {
+  nl.addEventListener("click", (e) => {
+    resetLinks();
+    resetHoverLines();
+    if (e.currentTarget) nl.classList.add("activeLink");
+    console.log("working");
   });
 });
